8435241471fc54d4ea21f3b66e3578e64d81d53e
Francisco Nievas <frannievas@gmail.com>
2017-04-02 12:06:54 -0300

    Changed regex for tokenizer

--- languagemodeling/scripts/train.py
+++ languagemodeling/scripts/train.py
@@ -11,9 +11,7 @@ Options:
 """
 from docopt import docopt
 import pickle
-
-from nltk.corpus import gutenberg
-
+from nltk.corpus import gutenberg, PlaintextCorpusReader, RegexpTokenizer
 from languagemodeling.ngram import NGram
 
 
@@ -21,8 +19,19 @@ if __name__ == '__main__':
     opts = docopt(__doc__)
 
     # load the data
-    sents = gutenberg.sents('austen-emma.txt')
-
+    # sents = gutenberg.sents('austen-emma.txt')
+    pattern = r'''(?ix)    # set flag to allow verbose regexps
+          (?:sr\.|sra\.)
+        | (?:[A-Z]\.)+        # abbreviations, e.g. U.S.A.
+        | \w+(?:-\w+)*        # words with optional internal hyphens
+        | \$?\d+(?:\.\d+)?%?  # currency and percentages, e.g. $12.40, 82%
+        | \.\.\.            # ellipsis
+        | [][.,;"'?():-_`]  # these are separate tokens; includes ], [
+    '''
+    tokenizer = RegexpTokenizer(pattern)
+    corpus = PlaintextCorpusReader('corpus/', '.*\.txt', word_tokenizer=tokenizer)
+    sents = corpus.sents()
+    
     # train the model
     n = int(opts['-n'])
     model = NGram(n, sents)
