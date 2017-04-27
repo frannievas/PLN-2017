3a7403c1d006a497629f66c41d9ed813ea38ab7a
Francisco Nievas <frannievas@gmail.com>
2017-04-27 17:33:27 -0300

    Added BaselineTagger

--- tagging/baseline.py
+++ tagging/baseline.py
@@ -1,12 +1,27 @@
-
+from collections import Counter, defaultdict
 
 class BaselineTagger:
 
     def __init__(self, tagged_sents):
         """
+        Programar un etiquetador baseline, que elija para cada palabra su etiqueta más frecuente observada en entrenamiento.
+        Para las palabras desconocidas, devolver la etiqueta 'nc0s000'.
+
         tagged_sents -- training sentences, each one being a list of pairs.
         """
-        pass
+
+        all_words = [word for sent in tagged_sents for word in sent]
+        counter = Counter(all_words)
+
+        max_counts = defaultdict(int)
+        self.taggs = taggs = defaultdict(str)
+        for w, t in counter.keys():
+            if max_counts[w] < counter[(w, t)]:
+                max_counts[w] = counter[(w, t)]
+                taggs[w] = t
+
+        self.unk_tagg = "nc0s000"
+        self.counts = max_counts
 
     def tag(self, sent):
         """Tag a sentence.
@@ -20,11 +35,14 @@ class BaselineTagger:
 
         w -- the word.
         """
-        return 'nc'
+
+        if self.unknown(w):
+            return self.unk_tagg
+        return self.taggs[w]
 
     def unknown(self, w):
         """Check if a word is unknown for the model.
 
         w -- the word.
         """
-        return True
+        return w not in self.counts
