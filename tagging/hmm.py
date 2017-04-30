f4a6dbc46f51c5ef31246cb21a18b9ee456db922
Francisco Nievas <frannievas@gmail.com>
2017-04-29 21:36:39 -0300

    Pass all test_hmm.py

--- tagging/hmm.py
+++ tagging/hmm.py
@@ -31,7 +31,7 @@ class HMM:
         prob = 0.0
 
         # No tags, empty tuple.
-        if self._n == 1:
+        if self.n == 1:
             prev_tags = ()
 
         if prev_tags in self.trans and tag in self.trans[prev_tags]:
@@ -60,16 +60,17 @@ class HMM:
         y -- tagging.
         """
         # TODO: Dado una secuencia de taggs devuelve su probabilidad
-        prob = 0.0
+
+        prob = 1.0
         n = self.n
-        prev_tags = self.start_tag*(n-1)
+        prev_tags = tuple(self.start_tag*(n-1))
         y_extended = y + self.end_tag
 
         for tag in y_extended:
             prob *= self.trans_prob(tag, prev_tags)
 
             # Delete 1 prev_tags and add the "tagg"
-            if self._n > 1:
+            if self.n > 1:
                 prev_tags = prev_tags[1:] + (tag,)
 
         return prob
@@ -83,7 +84,7 @@ class HMM:
         y -- tagging.
         """
 
-        prob = 0.0
+        prob = 1.0
         tagging_prob = self.tag_prob(y)
 
         if tagging_prob > 0:
@@ -100,14 +101,14 @@ class HMM:
         """
         log_prob = 0.0
         n = self.n
-        prev_tags = self.start_tag*(n-1)
+        prev_tags = tuple(self.start_tag*(n-1))
         y_extended = y + self.end_tag
 
         for tag in y_extended:
             prob = self.trans_prob(tag, prev_tags)
 
             # Delete 1 prev_tags and add the "tagg"
-            if self._n > 1:
+            if self.n > 1:
                 prev_tags = prev_tags[1:] + (tag,)
 
             if prob > 0:
@@ -145,6 +146,7 @@ class HMM:
 
         sent -- the sentence.
         """
+        return ViterbiTagger(self).tag(sent)
 
 
 class ViterbiTagger:
@@ -153,9 +155,48 @@ class ViterbiTagger:
         """
         hmm -- the HMM.
         """
+        self.hmm = hmm
+        self._pi = {}
 
     def tag(self, sent):
         """Returns the most probable tagging for a sentence.
 
         sent -- the sentence.
         """
+        hmm = self.hmm
+        n = self.hmm.n
+        self._pi = pi = {}
+        pi[0] = {tuple((n-1) * self.hmm.start_tag): (log2(1.0), [])}
+        m = len(sent)
+
+        for k in range(1, m+1):
+            pi[k] = {}
+
+            for tag in hmm.tagset:
+                # e(Xk|v)
+                e = hmm.out_prob(sent[k], tag)
+                if e > 0:
+                    for key in pi[k-1].keys():
+                        # q(v | w, t2...tn-1)
+                        q = hmm.trans_prob(tag, key)
+                        if q > 0:
+                            (p, tag_sent) = pi[k-1][key]
+                            new_prev = (key + (tag,))[1:]
+                            new_prob = p + log2(q) + log2(e)
+                            # MAX
+                            if (new_prev not in pi[k] or
+                                    new_prob > pi[k][new_prev][0]):
+                                pi[k][new_prev] = (new_prob, tag_sent + [tag])
+
+        # STOP
+        max_prob = float("-inf")
+        for key in pi[m].keys():
+            prob = hmm.trans_prob('</s>', key)
+            if prob > 0.0:
+                (p, tag_sent) = pi[k-1][key]
+                new_lp = prob + log2(p)
+                if new_lp > max_prob:
+                    max_prob = new_lp
+                    result = tag_sent
+
+        return result
