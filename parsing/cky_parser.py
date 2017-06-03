fa6458f0c5e464470e42efa42fbf36c96e6c7644
Francisco Nievas <frannievas@gmail.com>
2017-06-03 17:57:01 -0300

    Created Class CKYParser

---
+++ parsing/cky_parser.py
@@ -0,0 +1,12 @@
+class CKYParser:
+
+    def __init__(self, grammar):
+        """
+        grammar -- a binarised NLTK PCFG.
+        """
+
+    def parse(self, sent):
+        """Parse a sequence of terminals.
+
+        sent -- the sequence of terminals.
+        """
